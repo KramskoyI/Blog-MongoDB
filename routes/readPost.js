fc963ca5878a87b6ed3e2101defc149228f49688
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2022-01-20 18:46:17 +0300

    fix css and hbs files

--- routes/readPost.js
+++ routes/readPost.js
@@ -5,7 +5,7 @@ const  { Post } = require('../models/Post')
 /* GET Read Post page. */
 router.get('/readPost', async (req, res, ) => {
     const id = req.query.id;
-    const post = await Post.find({_id: id})
+    const post = await Post.find({_id: id}).populate('idAutor')
     res.render('readPost', {post})
 })
 
